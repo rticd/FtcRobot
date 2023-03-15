21dca66e586f2ea871c56e8829bdac54fb880b33
PhotoResistor <47359527+MrCarving@users.noreply.github.com>
2023-03-15 12:13:03 +0200

    Add files via upload

--- Autonomous/Actions/MoveForDistanceAction.java
+++ Autonomous/Actions/MoveForDistanceAction.java
@@ -7,28 +7,35 @@ import org.firstinspires.ftc.teamcode.Autonomous.Actions.IAction;
 import org.firstinspires.ftc.teamcode.Autonomous.RobotModel;
 import org.firstinspires.ftc.teamcode.Common.Coordinates;
 import org.firstinspires.ftc.teamcode.Common.DriveComponent;
+import org.firstinspires.ftc.teamcode.Common.Direction;
 
 public class MoveForDistanceAction extends BaseAction {
-    Coordinates initialCoordinates;
+    double displ;
     RobotModel model;
     DriveComponent driveComponent;
 
-    double distance;
     double prevTicks;
-    public double getDistance() {
-        return distance;
-    }
+    double robot_power = 1;
 
-    public MoveForDistanceAction(RobotModel model, DriveComponent driveComponent, double distance,
-                                 Telemetry telemetry) {
+    Direction dir;
+
+    Coordinates initialCoordinates;
+    //public double getDistance() {
+        //return distance;
+    //}
+
+    public MoveForDistanceAction(RobotModel model, DriveComponent driveComponent, double robot_power, double displ, Direction dir, Telemetry telemetry) {
         super(telemetry);
         this.model = model;
         this.driveComponent = driveComponent;
-        this.distance = distance;
+        this.robot_power = robot_power;
+        this.displ = displ;
+        this.dir = dir;
     }
 
     @Override
     public void start() {
+
         if(!finished) {
             //sets initial position to 0
             driveComponent.upperLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -36,19 +43,26 @@ public class MoveForDistanceAction extends BaseAction {
             driveComponent.upperRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             driveComponent.lowerRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-            driveComponent.lowerRight.setPower(1);
-            driveComponent.upperRight.setPower(1);
-            driveComponent.lowerLeft.setPower(1);
-            driveComponent.upperLeft.setPower(1);
+            driveComponent.lowerRight.setPower(robot_power);
+            driveComponent.upperRight.setPower(robot_power);
+            driveComponent.lowerLeft.setPower(robot_power);
+            driveComponent.upperLeft.setPower(robot_power);
 
+            int ticksToPosition = 0;
+            if (this.dir == Direction.Y){
+                ticksToPosition= -(int)(driveComponent.TICKS_PER_CM * this.displ);
+                driveComponent.lowerRight.setTargetPosition(ticksToPosition);
+                driveComponent.upperRight.setTargetPosition(ticksToPosition);
+                driveComponent.lowerLeft.setTargetPosition(ticksToPosition);
+                driveComponent.upperLeft.setTargetPosition(ticksToPosition);
+            } else if (this.dir == Direction.X){
+                ticksToPosition= -(int)(driveComponent.TICKS_PER_CM * this.displ);
+                driveComponent.lowerRight.setTargetPosition(ticksToPosition);
+                driveComponent.upperRight.setTargetPosition(-ticksToPosition);
+                driveComponent.lowerLeft.setTargetPosition(ticksToPosition);
+                driveComponent.upperLeft.setTargetPosition(-ticksToPosition);
+            }
             //sets new position
-            int ticksToPosition = -(int)(driveComponent.TICKS_PER_CM * distance);
-
-            driveComponent.lowerRight.setTargetPosition(ticksToPosition);
-            driveComponent.upperRight.setTargetPosition(ticksToPosition);
-            driveComponent.lowerLeft.setTargetPosition(ticksToPosition);
-            driveComponent.upperLeft.setTargetPosition(ticksToPosition);
-
             driveComponent.upperLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             driveComponent.lowerLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
             driveComponent.upperRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -61,16 +75,22 @@ public class MoveForDistanceAction extends BaseAction {
     @Override
     public void update() {
         if(!finished) {
-
-            int targetTicks = (int)(driveComponent.TICKS_PER_CM * distance);
+            int targetTicks = -(int)(driveComponent.TICKS_PER_CM * displ);
             int currentTicks = -driveComponent.upperLeft.getCurrentPosition();
-
             float cmTraveled = currentTicks / driveComponent.TICKS_PER_CM;
+
             //updating model
             double absAngleInRads = model.absAngle*Math.PI/180;
+            /*
             double x = cmTraveled * Math.cos(absAngleInRads);
             double y = cmTraveled * Math.sin(absAngleInRads);
-            Coordinates vector = new Coordinates(x, y);
+            */
+            Coordinates vector = new Coordinates(0,0);
+            if (this.dir == Direction.Y){
+                vector = new Coordinates(0, cmTraveled);
+            } else {
+                vector = new Coordinates(cmTraveled, 0);
+            }
             model.coordinates = Coordinates.add(initialCoordinates, vector);
             //checking if finished
             if(targetTicks == currentTicks && currentTicks == prevTicks) {
@@ -78,5 +98,10 @@ public class MoveForDistanceAction extends BaseAction {
             }
             prevTicks = currentTicks;
         }
+    }
+
+    @Override
+    public void exit() {
+
     }
 }
